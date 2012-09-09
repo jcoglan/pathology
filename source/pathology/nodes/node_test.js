aa62a90875e94d7ecf8e094d8733cc6c2226293a
James Coglan <jcoglan@gmail.com>
2012-09-09 12:11:59 +0100

    If a predicate atomizes to an empty string, it should be considered true.

--- source/pathology/nodes/node_test.js
+++ source/pathology/nodes/node_test.js
@@ -1,15 +1,10 @@
 Pathology.XPathParser.NodeTest = {
   evaluate: function(context, predicates, root, resultType, result) {
-    if (this.condition_name) {
-      switch (this.condition_name.textValue) {
-        case 'node':
-          // NOOP
-          break;
-        case 'text':
-          if (context.nodeType !== XPathResult.BOOLEAN_TYPE)
-            return;
-          break;
-      }
+    var name = this.condition_name;
+    if (name && name.textValue === 'node') {
+      // NOOP
+    } else if (name && name.textValue === 'text') {
+      if (context.nodeType !== XPathResult.BOOLEAN_TYPE) return;
     } else {
       var tagName = this.textValue.toLowerCase();
       if (tagName === '*') {
@@ -22,8 +17,8 @@ Pathology.XPathParser.NodeTest = {
     
     var viable = true;
     predicates.forEach(function(predicate) {
-      var expression = predicate.expression;
-      viable = viable && Pathology.atomize(expression, context, root);
+      viable = viable && Pathology.atomize(predicate.expression, context, root);
+      if (typeof viable === 'string') viable = true;
     });
     
     if (viable) result.push(context);
